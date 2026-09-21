1c80ec85d5a566766c6cd1e52a8c9785fd55438d
kiyu520 <2186925409@qq.com>
2026-09-21 22:58:23 +0800

    Complete Rustlings exercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,37 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let mut parts = s.split(',');
+        let name = match parts.next() {
+            Some(name) if !name.is_empty() => name,
+            _ => return Person::default(),
+        };
+
+        let age_str = match parts.next() {
+            Some(age) if !age.is_empty() => age,
+            _ => return Person::default(),
+        };
+
+        if parts.next().is_some() {
+            return Person::default();
+        }
+
+        let age = match age_str.parse::<usize>() {
+            Ok(age) => age,
+            Err(_) => return Person::default(),
+        };
+
+        Person {
+            name: name.to_string(),
+            age,
+        }
     }
 }
 
