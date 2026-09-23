63cef7230333903d9d25241aa033735e2b8231ad
kiyu520 <2186925409@qq.com>
2026-09-23 19:20:43 +0800

    Complete Rustlings exercises

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -2,7 +2,6 @@
 	heap
 	This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
 
 use std::cmp::Ord;
 use std::default::Default;
@@ -37,10 +36,18 @@ where
     }
 
     pub fn add(&mut self, value: T) {
-        //TODO
         self.items.push(value);
-        self.count+=1;
-        
+        self.count += 1;
+
+        let mut idx = self.count;
+        while idx > 1 {
+            let parent = self.parent_idx(idx);
+            if !(self.comparator)(&self.items[idx], &self.items[parent]) {
+                break;
+            }
+            self.items.swap(idx, parent);
+            idx = parent;
+        }
     }
 
     fn parent_idx(&self, idx: usize) -> usize {
@@ -60,8 +67,32 @@ where
     }
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
-        //TODO
-		0
+        let left = self.left_child_idx(idx);
+        if left > self.count {
+            return 0;
+        }
+
+        let right = self.right_child_idx(idx);
+        if right > self.count {
+            return left;
+        }
+
+        if (self.comparator)(&self.items[left], &self.items[right]) {
+            left
+        } else {
+            right
+        }
+    }
+
+    fn sift_down(&mut self, mut idx: usize) {
+        while self.children_present(idx) {
+            let child_idx = self.smallest_child_idx(idx);
+            if !(self.comparator)(&self.items[child_idx], &self.items[idx]) {
+                break;
+            }
+            self.items.swap(idx, child_idx);
+            idx = child_idx;
+        }
     }
 }
 
@@ -87,8 +118,24 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        //TODO
-		None
+        if self.count == 0 {
+            return None;
+        }
+
+        let result = std::mem::replace(&mut self.items[1], T::default());
+
+        if self.count == 1 {
+            self.items.pop();
+            self.count = 0;
+            return Some(result);
+        }
+
+        self.items[1] = std::mem::replace(&mut self.items[self.count], T::default());
+        self.items.pop();
+        self.count -= 1;
+        self.sift_down(1);
+
+        Some(result)
     }
 }
 
